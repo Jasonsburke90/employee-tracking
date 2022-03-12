296bdbe81e7b9691e23c43596e49127a6499dc77
unknown <jasonsburke90@gmail.com>
2022-03-11 20:33:06 -0600

    connection

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to employee database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
